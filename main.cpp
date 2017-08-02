d6bbe9982f4aaf811f3b1a92b9a9a9c6bec0013b
yujungchen <steiff.chen@gmail.com>
2017-08-02 22:49:51 +0800

    Add cache example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "template.h"
 
+
+
 int main(){
 
 	float a = 0.1f;
@@ -10,12 +12,22 @@ int main(){
 	int d = 2;
 	int e = 3;
 	int f = 0;
-	
+
 	c = getMax<float>(a, b);
 	f = getMax<int>(d, e);
 
 	printf("This is a template.\n");
 	printf("c = %f\n", c);
 	printf("f = %d\n", f);
+
+	CacheLine<SECTOR_NUM> Line;
+	Line.SetSector(0, 10);
+	Line.SetSector(1, 20);
+	int Data = 0;
+	Line.GetSector(0, Data);
+	printf("GetSector = %d\n", Data);
+
+	Cache< CacheLine<2> > m_Cache(2, 2);
+
 	return 0;
 }
--- template.h
+++ template.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <stdio.h>
-
+#define SECTOR_NUM 8
 
 
 template <class T>
@@ -12,3 +12,49 @@ T getMax(T &in0, T &in1){
 	else
 		return in1;
 }
+
+
+/*Define Cache Line Structure*/
+template <int N>
+class CacheLine{
+
+public:
+	CacheLine(){}
+	~CacheLine(){}
+	void SetSector(int _Sector, int _Data){
+		m_Data[_Sector] = _Data;
+	}
+	void GetSector(int _Sector, int &_Data){
+		_Data = m_Data[_Sector];
+	}
+
+private:
+	int m_Data[N];
+};
+
+
+/*Define Cache Structure*/
+template <typename T>
+class Cache{
+
+public:
+
+	Cache(int _Set, int _Way){
+		m_Set = _Set;
+		m_Way = _Way;
+
+		m_Cache = new T*[m_Way];
+		for(int i = 0 ; i < m_Way ; i++)
+			m_Cache[i] = new T[m_Set];
+	}
+
+
+	~Cache(){
+
+	}
+
+private:
+	T **m_Cache;
+	int m_Set;
+	int m_Way;
+};
